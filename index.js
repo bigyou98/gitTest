ca8ca1f1e090fc43f549bf015125bc52ab002325
sanghoon <bigyou00@gmail.com>
2022-08-22 15:02:38 +0900

    Feat : JSDoc 적용

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// 처음상태
+/**
+ * 사람이름 + age 반환 함수
+ * @todo jsdoc 익히기
+ * @param {string} name 사람 이름
+ * @param {number} age 사람 나이
+ * @returns {string} 사람이름 +  age
+ * @version 1.0.0
+ * @see https://naver.com
+ */
+const qwe = (name, age) => {
+  return name + age;
+};
+
+// JS Doc 이라고 함
+// 변수나 함수 위에 작성하여 설명할 수 있음
